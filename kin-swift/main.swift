d8f8596b69b9d8ded5aecf74cd8ef33899abbbe6
Davide Mendolia <davide@gokarumi.com>
2016-12-16 17:53:50 +0100

    Initial Commit

---
+++ kin-swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  kin-swift
+//
+//  Created by Davide Mendolia on 16/12/16.
+//  Copyright © 2016 Karumi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
